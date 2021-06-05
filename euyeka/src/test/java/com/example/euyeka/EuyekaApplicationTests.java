35867928300a9f17fa50b48a7f102c9098dd9a6f
IulianCom <48680137+IulianCom@users.noreply.github.com>
2021-06-05 20:59:14 +0300

    Add files via upload

---
+++ euyeka/src/test/java/com/example/euyeka/EuyekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.euyeka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuyekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
